756e9feae30e15788a49927c4c427d2512251694
imikieb <mikie.business99@gmail.com>
2022-05-01 13:25:27 -0300

    errors layer

---
+++ src/constants/httpCodes.ts
@@ -0,0 +1,4 @@
+export const httpSuccessCode = 200;
+export const httpCreatedCode = 201;
+export const httpNoContentCode = 204;
+export const httpInternalErrorCode = 500;
---
+++ src/constants/messages.ts
@@ -0,0 +1,4 @@
+export const defaultErrorMessage = 'Ocorreu um erro, tente novamente mais tarde.';
+export const createMessage = (field: string) => `${field} criado(a) com sucesso.`;
+export const editMessage = (field: string) => `${field} editado(a) com sucesso.`;
+export const deleteMessage = (field: string) => `${field} deletado(a) com sucesso.`;
